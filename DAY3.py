c673541d8a0356e1b24cbae07922bf87afcda099
Venkatkiran-Edu <84708828+Venkatkiran-Edu@users.noreply.github.com>
2021-05-25 00:03:13 +0530

    Add files via upload

---
+++ DAY3.py
@@ -0,0 +1,17 @@
+#DAY 3
+#AGE CALCULATOR
+current year = 2021
+birth year = int(input("Enter your birth year: "))
+age = current year - birth year
+print("Your current age is: " , age)
+
+#SIMPLE CALCULATOR
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print('Addition(a+b)=' , a+b)
+print('Subtraction(a-b)=' , a-b)
+print('Multiplication(a*b)=' , a*b)
+print('Division(a/b)' , a/b)
+print('Remainder=' , a%b)
+print('Float Division=', a//b)
+print('Power of input(a^b)=', a**b)
