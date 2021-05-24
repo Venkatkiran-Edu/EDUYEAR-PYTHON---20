098b9139b5a58edc849f2a323b9ed5d18267dc73
Venkatkiran-Edu <84708828+Venkatkiran-Edu@users.noreply.github.com>
2021-05-24 14:40:07 +0530

    Add files via upload

---
+++ DAY 2.py
@@ -0,0 +1,54 @@
+#DAY 2
+
+#integer as a
+a = 100
+print((a) , type(a))
+#to boolean
+b = bool(a)
+print(b)
+#to float
+b = float(a)
+print(b)
+#to string
+b = str(b)
+print (b)
+
+#float as c
+c = 100.2
+print( c , type(c))
+#to boolean
+d = bool(c)
+print(d)
+#to interger
+d = int(c)
+print(d)
+#to string
+d = str(c)
+print(d)
+
+#boolean as f
+f = True
+print(f , type(f))
+
+#to int
+g = int(f)
+print(g)
+#to float
+g = float(f)
+print(g)
+#to string
+g = str(f)
+print(g)
+
+#string as y
+y = "venkat"
+print( y , type(y))
+#to int
+z = int(y)
+print(z)
+#to boolean
+z = bool(y)
+print(z)
+#to float
+z = float(y)
+print(z)
